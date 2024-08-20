57924c5538c596fff7d349affe49fe82791be060
Pankaj Kumar <pkumar7@opentext.com>
2024-08-20 14:22:23 +0530

    terminal app

--- src/design_patterns/behavioral/observer/App.java
+++ src/design_patterns/behavioral/observer/App.java
@@ -1,7 +1,11 @@
 package design_patterns.behavioral.observer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Subject youtubeChannel = new YoutubeChannel();
         Observer observer = new Subscriber("Pankaj");
         Observer observer1 = new Subscriber("Kajal");
@@ -9,5 +13,18 @@ public class App {
         youtubeChannel.subscribe(observer);
 
         youtubeChannel.notifyAllObservers();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        while (true){
+//            int x = Integer.parseInt(bufferedReader.readLine());
+//            if(x == 1){
+//
+//            }else if(x == 2){
+//
+//            }else if(x == 3){
+//
+//            }else{
+//
+//            }
+//        }
     }
 }
